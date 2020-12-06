3c3886629570a3543ee6d1bd3eae4eeed342fd04
Nikolai.Volokhan <volohan1@mail.ru>
2020-12-07 04:01:47 +0500

    dead stones

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+import copy
 from player_colors import Colors
 from stones import Goishi
 
@@ -87,7 +88,87 @@ class Goban:
                     field.is_surrounded = False
         self.checks = set()
 
-    def score(self):
+    def find_territory(self, x, y, ignore_color):
+        if 0 < x <= self.size and 0 < y <= self.size:
+            field = self.get_field(x, y)
+            if not field or field.color == ignore_color:
+                try:
+                    color = field.color
+                except AttributeError:
+                    color = None
+                self.checks.add((x, y, color))
+                self.visited.add((x, y))
+                for i, j in self.shifts_to_neighbors:
+                    field = self.get_field(x + i, y + j)
+                    try:
+                        color = field.color
+                    except AttributeError:
+                        color = None
+                    if (x + i, y + j, color) not in self.checks:
+                        self.find_territory(x + i, y + j, ignore_color)
+
+    def try_to_play_to_capture(self, deep, color, moves):
+        if deep < 5:
+            res = True
+            if color != self.attack_color:
+                res &= self.try_to_play_to_capture(deep + 1, Colors(-color),
+                                                   copy.deepcopy(moves))
+            bad_moves = 0
+            for x, y in copy.deepcopy(moves).pop():
+                map = copy.deepcopy(self.map)
+                scores = copy.deepcopy(self.scores)
+
+                if self.make_move(x, y, color):
+                    res &= self.try_to_play_to_capture(deep + 1,
+                                                       Colors(-color),
+                                                       copy.deepcopy(moves))
+                else:
+                    bad_moves += 1
+
+                self.map = map
+                self.scores = scores
+            if bad_moves == len(moves):
+                return False
+            else:
+                return res
+        else:
+            return False
+
+    def find_and_finish_playing_conflict_zones(self, x, y, attack_color):
+        self.checks = set()
+        self.find_territory(x, y, Colors(-attack_color))
+        if len(self.checks) < self.size * self.size / 2:
+            original_position = copy.deepcopy(self.checks)
+            possibility_moves = set()
+            for x, y, color in self.checks:
+                if not color:
+                    possibility_moves.add((x, y))
+            if len(original_position) != len(possibility_moves):
+                self.attack_color = attack_color
+                self.try_to_play_to_capture(0, attack_color,
+                                            possibility_moves)
+
+    def take_dead_stones(self):
+        self.visited = set()
+        for y in range(self.size):
+            for x in range(self.size):
+                if (x, y) not in self.visited and not self.get_field(x, y):
+                    self.find_and_finish_playing_conflict_zones(x, y,
+                                                                Colors.white)
+                    self.find_and_finish_playing_conflict_zones(x, y,
+                                                                Colors.black)
+
+    def to_tile_neutral_field(self):
+        pass
+
+    def add_captured_territory(self):
+        pass
+
+    def score(self, game_is_end):
+        if game_is_end:
+            self.take_dead_stones()
+            self.to_tile_neutral_field()
+            self.add_captured_territory()
         return self.scores
 
     def get_map(self):
@@ -100,7 +181,7 @@ class Goban:
                     res += field.image
                 else:
                     res += '+'
-                    #res += '✛'
+                    # res += '✛'
             res += '\n'
         res += '  '
         for i in range(self.size):
